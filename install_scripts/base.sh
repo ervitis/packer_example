10603a7bb9e889904b4b34cce479ebb308cc42c6
beeva-victormartin <victor.martin@beeva.com>
2015-11-01 19:53:14 +0100

    end packer image build

--- install_scripts/base.sh
+++ install_scripts/base.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
-yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl git
+yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl git python python-devel python-setuptools
+
+easy_install pip
+pip install -U ansible
