f62d699aedac76c9fbb063b2b2f3871f91a016b8
ervitis <vitomarti@gmail.com>
2016-02-07 00:04:37 +0100

    fix: PATH and some services

--- install_scripts/base.sh
+++ install_scripts/base.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
-yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl git python python-devel python-setuptools
+yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl git python python-devel python-setuptools vim
 
 easy_install pip
 pip install -U ansible
