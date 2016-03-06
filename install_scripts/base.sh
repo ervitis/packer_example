2c78559180394380d8fcd8d4cd9a03a5d7a07c15
ervitis <vitomarti@gmail.com>
2016-03-06 23:24:49 +0100

    feat: add more requirements

--- install_scripts/base.sh
+++ install_scripts/base.sh
@@ -2,6 +2,7 @@
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl git python python-devel python-setuptools vim
+yum -y install kernel-debug-devel autoconf automake rpm-build redhat-rpm-config wget kernel-devel
 
 easy_install pip
 pip install -U ansible
